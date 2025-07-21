c665d309c2699ac037b9b0b55ef96305f6e48c1d
Xapsiel <ilyas_useinov3301@mail.ru>
2025-07-21 11:22:46 +0300

    l1/5

--- l1/5/main.go
+++ l1/5/main.go
@@ -9,15 +9,15 @@ import (
 )
 
 func Generator(ctx context.Context) chan int {
-	in := make(chan int)
+	in := make(chan int) // канал для генерации
 	go func() {
 		for {
 			select {
 			case <-ctx.Done():
-				close(in)
+				close(in) // закрытие канала
 				return
 			default:
-				n := rand.Intn(100)
+				n := rand.Intn(100) // имитация полезной нагрузки
 				in <- n
 			}
 		}
@@ -27,14 +27,14 @@ func Generator(ctx context.Context) chan int {
 }
 
 func main() {
-	delay := flag.Duration("delay", 10*time.Second, "delay between task generations")
+	delay := flag.Duration("delay", 10*time.Second, "delay for stopping program") //продолжительность работы программы
 	flag.Parse()
 
-	ctx, cancel := context.WithTimeout(context.Background(), *delay)
+	ctx, cancel := context.WithTimeout(context.Background(), *delay) //контекст с таймаутом
 	defer cancel()
 	result := Generator(ctx)
 	for n := range result {
-		fmt.Println(n)
+		fmt.Println(n) //чтение из канала. При закрытии - цикл завершится
 	}
 	fmt.Println("Время отправки в канал истекло")
 
