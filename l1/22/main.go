0cfec9eff931faa82d3b5ee5921b6bfe3fb9ed66
Xapsiel <ilyas_useinov3301@mail.ru>
2025-07-22 12:25:29 +0300

    l1/22

---
+++ l1/22/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func performBigOperations(a, b *big.Int) {
+	sum := new(big.Int).Add(a, b)
+	fmt.Printf("Сумма: %s + %s = %s\n", a, b, sum)
+
+	diff := new(big.Int).Sub(a, b)
+	fmt.Printf("Разность: %s - %s = %s\n", a, b, diff)
+
+	prod := new(big.Int).Mul(a, b)
+	fmt.Printf("Произведение: %s * %s = %s\n", a, b, prod)
+
+	if b.Cmp(big.NewInt(0)) == 0 {
+		fmt.Println("Деление: деление на ноль")
+	} else {
+		quot := new(big.Int).Div(a, b)
+		fmt.Printf("Деление: %s / %s = %s\n", a, b, quot)
+	}
+}
+
+func main() {
+	a := big.NewInt(1 << 62)
+	b := big.NewInt(1 << 42)
+	fmt.Printf("Числа: a = %s, b = %s\n", a, b)
+	performBigOperations(a, b)
+}
